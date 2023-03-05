123c4e939646e646aff7bffdb547abe192b11f01
Coding-Cactus <codingcactus.cc@gmail.com>
2023-03-05 17:09:46 +0000

    :sparkles: Add projects

--- scroll.js
+++ scroll.js
@@ -1,10 +1,21 @@
-function cactus() {
+function scroll() {
     if (window.scrollY === 0)
         document.querySelector("#name").classList.remove("scroll");
     else {
         document.querySelector("#name").classList.add("scroll");
     }
+
+    document.querySelectorAll("#projects > div").forEach(project => {
+        if (
+            document.documentElement.scrollTop !== 0 &&
+            document.documentElement.scrollTop >= project.offsetTop - 0.5 * window.screen.height
+        ) {
+            project.classList.add("show");
+        } else {
+            project.classList.remove("show");
+        }
+    });
 }
 
-window.onload = cactus;
-document.addEventListener("scroll", cactus);
+window.addEventListener("load", scroll);
+document.addEventListener("scroll", scroll);
